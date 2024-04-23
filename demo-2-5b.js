69124ef5761d26ff4dc41b64f1e69e36b6b7dc03
Mireya Avila <mireyaavila147@yahoo.com>
2024-04-23 09:28:15 -0400

    did some demo exercises

---
+++ demo-2-5b.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var firstNum = 7;
+
+var toThePowerOf = 2;
+
+var answer = Math.pow(firstNum, toThePowerOf);
+
+console.log(answer);
+
+console.log(Math.pow(7,2));
+
+
+console.log(7**2);
+
