2f6bf95d2dfeee4563de50d3fe031c9311bc35dc
Mireya Avila <mireyaavila147@yahoo.com>
2024-04-24 10:02:30 -0400

    made new demos

---
+++ demo-2-27.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var x = "123";
+var y = "456";
+var z = x + y;
+console.log(z);
+
+//----------------------
+
+var x = "123";
+var y = "456";
+
+x = parseInt(x);
+y = parseInt(y);
+
+var z = x + y;
+console.log(z);
